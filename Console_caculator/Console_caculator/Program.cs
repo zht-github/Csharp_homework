38a70a65d93c1f44bc70ca1f9ee93435d081a5be
海涛 张 <374887477@qq.com>
2021-03-09 20:06:52 +0800

    为控制台计算器做出调整

--- Console_caculator/Console_caculator/Program.cs
+++ Console_caculator/Console_caculator/Program.cs
@@ -6,7 +6,7 @@ namespace Console_caculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input your parameters and operator as:1 + 1");
+            Console.WriteLine("Please input your parameters and operator as:1 + 1 \n 请一定用空格隔开不然识别不了！");
             /*
             int para1 = Console.Read() - '0';
             Console.Read();
@@ -15,18 +15,18 @@ namespace Console_caculator
             int para2 = Console.Read() - '0';
             Console.WriteLine("{0} {1} {2}", para1, op, para2);
             */
-            char[] charArray = Console.ReadLine().ToCharArray();
-            Console.WriteLine("My input string's length is {0}", charArray.GetLength(0));
-            int para1 = charArray[0] - '0';
-            char op = charArray[2];
-            int para2 = charArray[charArray.Length - 1] - '0';
+            char[] opArray = new char[] { ' ' };
+            string[] arr = Console.ReadLine().Split(opArray);
+            double para1 = double.Parse(arr[0]);
+            char op = arr[1].ToCharArray()[0];
+            double para2 = double.Parse(arr[2]);
 
             switch (op)
             {
-                case '+': Console.WriteLine("result={0}!", para1 + para2); break;
-                case '-': Console.WriteLine("result={0}!", para1 - para2); break;
-                case '*': Console.WriteLine("result={0}!", para1 * para2); break;
-                case '/': Console.WriteLine("result={0}!", para1 / para2); break;
+                case '+': Console.WriteLine("result={0}", para1 + para2); break;
+                case '-': Console.WriteLine("result={0}", para1 - para2); break;
+                case '*': Console.WriteLine("result={0}", para1 * para2); break;
+                case '/': Console.WriteLine("result={0}", para1 / para2); break;
                 default: Console.WriteLine("Operation input error!"); break;
             }
 
