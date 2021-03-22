46a005c2aceb6b8984356ff0c95647b282b7b6cf
海涛 张 <374887477@qq.com>
2021-03-22 23:20:00 +0800

    update

---
+++ homework_No3/homework3_clock/homework3_clock/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace homework3_clock
+{
+    delegate void TickAction(object sender, TickArgs args);
+    delegate void AlarmAction(object sender, AlarmArgs args);
+    class TickArgs : EventArgs
+    {
+        public String Sound;
+        public TickArgs(String sound){
+            this.Sound = sound;
+        }
+    }
+
+    class AlarmArgs : EventArgs
+    {
+        public static String Sound = "Ding,Ding.....";//默认铃声
+        public String Todo { get; set; }
+        public int Oclock { get; set; }//闹钟默认只能整点报时
+        public AlarmArgs(String todo,int time)
+        {
+            Todo = todo;
+            Oclock = time;
+        }
+    }
+    class Clock {
+        public event TickAction TickHandler;
+        public event AlarmAction AlarmHandler;
+
+        public void Alarm(String todo,int time)
+        {
+            Console.WriteLine(AlarmArgs.Sound);
+            AlarmHandler(this, new AlarmArgs(todo, time));
+        }
+        public void Tick(String sound)
+        {
+            TickHandler(this, new TickArgs(sound));
+        }
+        public Clock()
+        {
+            Initializer();
+        }
+        void Initializer()
+        {
+            this.AlarmHandler += InformTime;
+            this.AlarmHandler += InformToDo;
+            this.TickHandler += Ticking;
+        }
+        public void InformTime(object sender, AlarmArgs args)
+        {
+            Console.WriteLine($"It's {args.Oclock} O'clock now!");
+        }
+        public void InformToDo(object sender, AlarmArgs args) {
+            Console.WriteLine($"Notice! You should {args.Todo} right now!");
+        }
+        public void Ticking(object sender, TickArgs args)
+        {
+            Console.WriteLine(args.Sound);
+        }
+        
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Clock myClock = new Clock();
+            myClock.Alarm("DO HOMEWORK", 10);
+            Console.WriteLine("=========================");
+            myClock.Tick("Tick,Tick...");
+        }
+    }
+}
---
+++ homework_No3/homework3_list/homework3_list/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace homework3_list
+{
+    public class Node<T>
+    {
+        public T data { get; set; }
+        public Node<T> next;
+        public Node(T t)
+        {
+            data = t;
+        }
+    } 
+    public class MyList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public MyList()
+        {
+            head = tail = null;
+        }
+        public void add(T data)
+        {
+            if (head == null)
+            {
+                head = tail = new Node<T>(data);
+            }
+            else
+            {
+                Node<T> temp = new Node<T>(data);
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> presentNode = head;
+            while (presentNode != null)
+            {
+                action(presentNode.data);
+                presentNode = presentNode.next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            MyList<int> myList = new MyList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                myList.add(i);
+            }
+            //输出表
+            myList.ForEach(x => Console.Write(x+" "));
+            Console.WriteLine("\n==========================");
+            //求最大值
+            int max = 0;
+            myList.ForEach(x =>
+            {
+                if (max < x) max = x;
+            });
+            Console.WriteLine($"Max item is {max}");
+            Console.WriteLine("=============================");
+            //求最小值
+            int min = 0;
+            myList.ForEach(x =>
+            {
+                if (min > x) min = x;
+            });
+            Console.WriteLine($"Min item is {min}");
+            Console.WriteLine("=============================");
+            //求和
+            int sum = 0;
+            myList.ForEach(x =>
+            {
+                sum += x;
+            });
+            Console.WriteLine($"Sum item is {sum}");
+            Console.WriteLine("=============================");
+        }
+    }
+}
